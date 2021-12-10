0c1c937905af4c6677434d6a1c4e5d77e4edceb1
AhnafAhamed <ahnafahamed4@gmail.com>
2021-12-10 16:05:32 +0530

    feat: add security features

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const { requestLogger, errorLogger } = require('./middleware/logger');
+const validateUrl = require('./utils/validateUrl');
 const users = require('./routes/users');
 const { createCard } = require('./controllers/cards');
 const cards = require('./routes/cards');
@@ -16,14 +22,52 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
 
 app.use(express.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
-// app.use(auth);
+app.use(cors());
+app.options('*', cors());
+app.use(helmet());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validateUrl),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.use(auth);
 app.use('/', users);
 app.use('/', cards);
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'An error occurred on the server'
+        : message,
+    });
+  next();
 });
 
 app.post('/cards', auth, createCard);
